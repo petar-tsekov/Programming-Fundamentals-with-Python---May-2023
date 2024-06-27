ff69fc35021eb6c32854b9cc8b671c5d11b63418
petar-tsekov <165672876+petar-tsekov@users.noreply.github.com>
2024-06-27 19:45:49 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/integer_operations_01.py
@@ -0,0 +1,10 @@
+first_integer = int(input())
+second_integer = int(input())
+third_integer = int(input())
+fourth_integer = int(input())
+
+first_sum = first_integer + second_integer
+second_sum = first_sum // third_integer
+final_sum = second_sum * fourth_integer
+
+print(final_sum)
